cfc1073a1c9008e043ecedcc2178d188f8b384d0
Sadip Giri <sadipgiri@bennington.edu>
2019-03-02 22:48:40 -0500

    level 1 and 2 complete

---
+++ my_clean_count_stems.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Python3 program to clean and count stems from text file creating Porter Stemmer from scratch
+    Author: Sadip Giri (sadipgiri@bennington.edu)
+    Date: 03/02/2019
+"""
+
+import regex as re
+
+def porter_stemmer(word):
+    pass
